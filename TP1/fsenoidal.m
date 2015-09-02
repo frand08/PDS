7b6682d5532549283e10d5961c87fe454f7b5823
frand08 <dominguezfranciscoa@gmail.com>
2015-09-02 00:30:40 -0300

    Agregados que son cada input y output

--- TP1/fsenoidal.m
+++ TP1/fsenoidal.m
@@ -1,4 +1,11 @@
 function y = fsenoidal(A,fo,phase,offset,N,fs)
+% A: amplitud (input)
+% fo: frecuencia de la onda (input)
+% phase: desfazaje (input)
+% offset: nivel de continua (input)
+% N: numero de muestras (input)
+% fs: frecuencia de muestreo (input)
+% y: senoidal (output)
     n=0:N-1;
     t = n/fs;
     sal = offset + A*sin(2*pi*fo*t + phase);
