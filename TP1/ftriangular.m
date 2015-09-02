f8fe772c521fd0179b337ca6c0b11a23bac10ae0
frand08 <dominguezfranciscoa@gmail.com>
2015-09-02 00:38:12 -0300

    Agregados los parametros de entrada y de salida de la funcion

---
+++ TP1/ftriangular.m
@@ -0,0 +1,13 @@
+function y = ftriangular(A,fo,sym,offset,N,fs)
+% A: amplitud (input)
+% fo: frecuencia de la onda (input)
+% sym: punto de simetria en porcentaje de 0 a 1 (input)
+% offset: nivel de continua (input)
+% N: numero de muestras (input)
+% fs: frecuencia de muestreo (input)
+% y: triangular (output)
+    
+    
+    
+    
+end
